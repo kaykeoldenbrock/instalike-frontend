5d694d443cc665f5e4395cc4b81e6ac28413c40f
Kayke Ferreira Oldenbrock <kaykeferreiraoldenbrock@gmail.com>
2026-02-28 23:52:30 -0300

    Meu primeiro deploy do front

---
+++ assets/js/fetchApis.js
@@ -0,0 +1,11 @@
+let { API_URL } = process.env;
+
+export default async function fetchImages() {
+  try {
+    const response = await fetch(process.env.API_URL);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Erro ao buscar dados:", error);
+  }
+}
